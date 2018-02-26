b676a1007f75d2dfc9b1e64ec8c6e5d07ad33a36
Torsten Curdt <tcurdt@vafer.org>
2018-02-26 13:38:21 +0100

    text for every language

--- source/Localization.h
+++ source/Localization.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define LS_DEMO_HELLO "Hallo!"
+#define LS_DEMO_HELLO "Hi!"
 #define LS_DEMO_BUTTON_A "A"
 #define LS_DEMO_BUTTON_B "B"
 #define LS_DEMO_BUTTON_AB "A+B"
-#define LS_DEMO_SHAKE "SCHUETTELN"
-#define LS_DEMO_GREAT "SUPER!"
+//#define LS_DEMO_SHAKE "SCHUETTELN"
+#define LS_DEMO_GREAT "OK!"
