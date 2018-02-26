efeb1803af7bd2ae6f4104f90d8d14715cecea48
Torsten Curdt <tcurdt@vafer.org>
2018-02-26 15:00:33 +0100

    being lazy

--- contrib/copy.sh
+++ contrib/copy.sh
@@ -4,10 +4,10 @@ SRC=../../calliope_playbook_mini/source/
 DST=`pwd`/source/
 
 rsync -rtuv --progress $SRC $DST --exclude Main.*
-#rsync -rtuv --progress $DST $SRC --exclude Main.*
+# rsync -rtuv --progress $DST $SRC --exclude Main.* --exclude Run*.* --exclude Localization.* --exclude Utils*.* --exclude Menu.* --exclude Storage.*
 
 SRC=../../calliope_playbook_mini/contrib/
 DST=`pwd`/contrib/
 
-rsync -rtuv --progress $SRC $DST
-#rsync -rtuv --progress $DST $SRC
+# rsync -rtuv --progress $SRC $DST
+# rsync -n -rtuv --progress $DST $SRC
