053f940c93ea0603c342f0c07ba4e3b8679904cd
Torsten Curdt <tcurdt@vafer.org>
2018-02-26 13:37:19 +0100

    have log and memsum

--- contrib/log.sh
+++ contrib/log.sh
@@ -4,6 +4,5 @@
 ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 DEVICE=/dev/cu.usbmodem1422
 
-#( stty speed 115200 cs8 1>/dev/null 2>&1; cat ) <$DEVICE
+( stty speed 115200 cs8 1>/dev/null 2>&1; cat ) <$DEVICE
 #( stty speed 115200 cs8 1>/dev/null 2>&1; hexdump -C ) <$DEVICE
-( stty speed 115200 cs8 1>/dev/null 2>&1; python2 $ROOT/memsum.py Calliope) <$DEVICE
