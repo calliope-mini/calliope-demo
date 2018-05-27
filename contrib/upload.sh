e8ad2d0327e30b3e46d4595ef127f7ae379b18bf
Torsten Curdt <tcurdt@vafer.org>
2018-05-27 19:37:43 +0200

    copy script cleanup

--- contrib/upload.sh
+++ contrib/upload.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
 cp build/calliope-mini-classic-gcc/source/*-combined.hex /Volumes/MINI/
-
-if [ -x $HOME/Shared/Manolis ]; then
-  cp build/calliope-mini-classic-gcc/source/*-combined.hex $HOME/Shared/Manolis
-fi
